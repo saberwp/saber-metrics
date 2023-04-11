a099b5d54c75b6ac0200c20b523b172523c1c248
Casey Milne <casey@saberwp.com>
2023-04-11 20:59:37 +0200

    Draft report.

--- templates/section-metric-log.php
+++ templates/section-metric-log.php
@@ -48,3 +48,29 @@
 		</tbody>
 	</table>
 </section>
+
+<!-- Reporting -->
+<?php
+
+$results = \SaberMetrics\MetricLog::fetch();
+if( ! empty( $results )) {
+	$total = 0;
+	foreach ($results as $result) {
+		$total += $result->value;
+	}
+}
+
+$count = count( $results );
+
+$average = 0;
+if( $total > 0 && $count > 0 ) {
+	$average =  $total / $count;
+}
+
+echo '<div>';
+echo '<h2>Count: ' . $count . '</h2>';
+echo '<h2>Total: ' . number_format( $total ) . '</h2>';
+echo '<h2>Average: ' . number_format( $average ) . '</h2>';
+echo '</div>';
+
+?>
