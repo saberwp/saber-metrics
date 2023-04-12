d529478f75088dc9257f69c19d9445abe7ca252e
Casey Milne <casey@saberwp.com>
2023-04-12 18:32:17 +0200

    Add grouping filter.

--- templates/components/report-filters.php
+++ templates/components/report-filters.php
@@ -8,9 +8,23 @@
 	</div>
 	<div class="sm-report-filter">
 		<label>Time Period</label>
-		<select>
+		<select id="sm-report-filter-time-period">
 			<option value="1">Today</option>
 			<option value="2">Weekly</option>
+			<option value="4">Monthly</option>
+			<option value="5">Quarterly</option>
+			<option value="6">Annually</option>
+		</select>
+	</div>
+	<div class="sm-report-filter">
+		<label>Grouping</label>
+		<select id="sm-report-filter-grouping">
+			<option value="1">Log</option>
+			<option value="2">Daily</option>
+			<option value="3">Weekly</option>
+			<option value="4">Monthly</option>
+			<option value="5">Quarterly</option>
+			<option value="6">Annually</option>
 		</select>
 	</div>
 </section>
